9fe1af08b2026636e5683feb2054b58b34c2bef5
Kelly Farran <klfarran@comcast.net>
2024-10-02 15:41:32 -0400

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //Main program for Scotland Yard AI Group Project
-//Kelly Farran, Kennedy Watkins, Emmet Richie 
+//Kelly Farran, Kennedy Watkins, Emmet Ritchie 
 //CS340 Fall 2024
 
 #include <iostream>
