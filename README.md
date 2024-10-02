04c48df5d0b068a4dddd73054afdccc4ed61f1fd
Kelly Farran <klfarran@comcast.net>
2024-10-02 15:22:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Scotland Yard Project
+## Created by: Emmet Richie, Kennedy Watkins, and Kelly Farran
+
+Talk about project here 
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Main program for Scotland Yard AI Group Project
+//Kelly Farran, Kennedy Watkins, Emmet Richie 
+//CS340 Fall 2024
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+	int main() {
+		
+ 
+	}
