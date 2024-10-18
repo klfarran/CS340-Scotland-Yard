b9c652998827825baee36db68a1227ec5e36547d
Kennedy Watkins <37742673+kenamazing@users.noreply.github.com>
2024-10-18 15:14:26 -0400

    Add files via upload

--- Ticket.cpp
+++ Ticket.cpp
@@ -17,12 +17,13 @@ int getIndex(string& ticket) {
 // and the Constructor makes tickets for all players
 Ticket::Ticket(int numPlayers) {
 	
-	//set size of private playerTickets around now
-    // plays have 3 tickets, taxi, bus, under, plus black ticket now
-    playerTickets.resize(numPlayers, vector<int>(4, 0)); //i need ticket counts for player starting at 0
+//set size of private playerTickets around now
+// plays have 3 tickets, taxi, bus, under, plus black ticket now
+//number of players x 4 types of tickets
+ playerTickets.resize(numPlayers, vector<int>(4, 0)); //i need ticket counts for player starting at 0
 
     // set tickets for detective players (except Mr. X)
-    for (int i = 0; i < numPlayers - 1; ++i) {
+for (int i = 0; i < numPlayers - 1; ++i) {
 		//player i is given 3 tickets 
         playerTickets[i][0] = 10;  // 10 t tickets
 		
@@ -43,23 +44,28 @@ Ticket::Ticket(int numPlayers) {
 	playerTickets[numPlayers - 1][3] = 4;  // 4 s moves
 }
 
-// ticket for a specific player
-//(paras: what player, reference ticket) player with id num?
+// checticket for a specific player
+    //(paras: what player, reference ticket) player with id num?
 bool Ticket::useTicket(int playerId, string& ticketType) {
 	//call index function to store the right ticket 
-    int ticketIndex = getIndex(ticket);  // Find the right ticket
+    int ticketIndex = getIndex(ticket);  // Find the right ticket\
+	
 	//if the ticket is valid and checks if ticket is avail for player with id
     if (ticketIndex != 0 && playerTickets[playerId][ticketIndex] > 0) {
-        playerTickets[playerId][ticketIndex]--;  // 
+       
+	   playerTickets[playerId][ticketIndex]--;  // 
         return true;  // Ticket used successfully
-    } else {
-        cout << "Player " << playerId << " is out of " << ticketType << " tickets. Oh No!"<<endl;
+    } 
+	else {
+    cout << "Player " << playerId << " is out of " << ticketType << " tickets. Oh No!"<<endl;
         return false;  // No tickets left
     }
+	
 }
 ////
 // for now cout what ticket srae left ithink for just t
 void Ticket::TicketAvail() {
+	
 	//loop through amount of tickets 
     for (int i = 0; i < playerTickets.size(); i++) {
         cout << "PLAYER: " << i <<endl;
@@ -67,5 +73,5 @@ void Ticket::TicketAvail() {
         cout << "BUS: " << playerTickets[i][1] <<endl;
         cout << "UNDERGROUND: " << playerTickets[i][2] <<endl;
 		cout << "BLACK: " <<playerTickets[i][3] <<endl;
-    }
+    }//for loop
 }
