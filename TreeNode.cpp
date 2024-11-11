a325549fb38b361d4f533532f081555ae36fef6b
Kelly Farran <klfarran@comcast.net>
2024-11-11 17:46:25 -0500

    Updated TreeNode class and added creation of possible mrX locations tree to GameManager

--- TreeNode.cpp
+++ TreeNode.cpp
@@ -4,47 +4,64 @@
 
 using namespace std;
 
-	// getters and setters for treenode class
-	vector<TreeNode*> TreeNode::getChildren() const{
-		return children;
+	//Constructors
+	//Explicit constructor 
+	TreeNode::TreeNode(Station* inStation, vector<TreeNode> inChildren) {
+		station = inStation;
+		children = inChildren;
 	}
 	
-	void TreeNode::setChildren(vector<TreeNode*> inChildren) {
-		children = inChildren;
+	//Default constructor
+	TreeNode::TreeNode() {
+		
+	}
+
+	//Getters
+	vector<TreeNode> TreeNode::getChildren() const{
+		return children;
 	}
 	
 	int TreeNode::getNumChildren() const{
 	  return children.size();
 	}
 
-	void TreeNode::setChild(int pos, TreeNode* T){
+	TreeNode TreeNode::getChild(int stationNumber) const{
+	  return children[stationNumber];
+	}
+	
+		Station* TreeNode::getStation() const{
+	  return station;
+	}
+	
+	void TreeNode::getLeaves(TreeNode root, vector<TreeNode>& leaves) const { //leaves is initially empty
+		if(root.isLeaf()) //root is a leaf
+			leaves.push_back(root); 
+		else {
+			for(TreeNode child : root.getChildren()) {
+				getLeaves(child, leaves);
+			}
+		}
+
+	}
+	
+	
+	//Setters
+		void TreeNode::setChildren(vector<TreeNode> inChildren) {
+		children = inChildren;
+	}
+	
+		void TreeNode::setChild(int pos, TreeNode T){
 	  children[pos] = T;
 	}
 	
-	TreeNode* TreeNode::getChild(int stationNumber) const{
-	  return children[stationNumber];
-	}
-
 	void TreeNode::setStation(Station* newStation){
 	  station = newStation;
 	}
 
-	Station* TreeNode::getStation() const{
-	  return station;
-	}
-
+	
+	//Other functions
 	bool TreeNode::isLeaf() {
 		return children.empty(); //returns true if children is empty, false otherwise (a TreeNode is a leaf if it has no children)
 	}
 
-	vector<TreeNode*> TreeNode::getLeaves(TreeNode* root, vector<TreeNode*>& leaves) const { //leaves is initially empty
-		if(root->getChildren().empty()) //root is a leaf
-			leaves.push_back(root); 
-		else {
-			for(TreeNode* child : root->getChildren()) {
-				getLeaves(child, leaves);
-			}
-		}
-		
-	  return leaves;
-	}
+	
